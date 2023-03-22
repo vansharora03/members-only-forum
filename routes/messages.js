5389910d12a5b206b29af3cbb48c37d6d18f493b
vansharora03 <vansh1993@icloud.com>
2023-03-22 19:12:56 -0400

    Sign-up GET

--- routes/messages.js
+++ routes/messages.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
 
+/** GET Messages */
 router.get('/', messageController.messages_list);
 
+/** GET Sign-up */
+router.get('/sign-up', messageController.sign_up_GET);
 
 module.exports = router;
